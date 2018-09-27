0972a9efef9495d1478e1ba04d1621d820188439
DayanaChris <41943828+DayanaChris@users.noreply.github.com>
2018-09-27 20:41:49 +0800

    Add files via upload

--- application/models/Query_model.php
+++ application/models/Query_model.php
@@ -21,4 +21,40 @@ class Query_model extends CI_Model
 				->get();
 		return $query;
 	}
+
+	public function questioner($id, $catId){
+		$qury = $this->db->select('*, quiz.id as quizID')
+										 ->from('level')
+										 ->join('quiz', 'level.id = quiz.level_id')
+										 ->where('level.id', $id)
+										 ->where('quiz.category_id', $catId)
+										 ->order_by('rand()')
+										 ->limit(1)
+										 ->get();
+	return $qury;
+	}
+	public function get_answer($id){
+		$query = $this->db->select()
+									 		 ->from('quiz_image')
+											 ->join('images', 'quiz_image.img_id = images.id')
+											 ->where('quiz_image.quiz_id', $id)
+											 ->order_by('rand()')
+											 ->get();
+			return $query;
+	}
+
+
+
+	// public function videoupld($url)
+  //   {
+  //     $title = $this->input->post('video_image');
+  //     //$details = $this->input->post('details');
+  //     //$type = $this->input->post('gallery');
+  //     $data = array(
+  //       'title'  => $title,
+  //       'url' => $url,
+  //       //'category'  => $type`
+  //     );
+  //     $this->db->insert('videoss', $data);
+  //   }
 }
